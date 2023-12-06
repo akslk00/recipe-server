cb44f0b9da355e8050bae63b76b7b91c93965003
akslk00 <akakquswodud31@gmail.com>
2023-12-06 17:39:04 +0900

    add

--- app.py
+++ app.py
@@ -3,7 +3,8 @@
 from flask import Flask
 from flask_restful import Api
 
-from resources.recipe import RecipeListResource, RecipeResource
+from resources.recipe import RecipeListResource, RecipePublishResource, RecipeResource
+from resources.user import UserRegisterResource
 
 app = Flask(__name__)
 
@@ -12,9 +13,12 @@ api = Api(app)
 # API를 구분해서 실행시키는 것은
 # HTTP METHOD 와 URL 의 조합이다
 # 경로(path)와 리소스(API코드)를 연결한다.
-api.add_resource( RecipeListResource ,'/recipes')
+api.add_resource(RecipeListResource,'/recipes')
 #api.add_resource(,'/recipes/레시피테이블의 아이디')
 api.add_resource(RecipeResource,'/recipes/<int:recipe_id>')
 
+api.add_resource(RecipePublishResource,'/recipes/<int:recipe_id>/publish')
+
+api.add_resource(UserRegisterResource,'/user/register')
 if __name__ == '__main__':
     app.run()
